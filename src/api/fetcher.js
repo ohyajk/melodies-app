943689b44441a80539cb7e7e4bd58c6480b86e6d
Jitender Kumar <ohyajk@gmail.com>
2024-07-16 12:26:47 +0530

    Update fetcher.jsc2

--- src/api/fetcher.js
+++ src/api/fetcher.js
@@ -2,49 +2,49 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // Fetch All Search Query
 export const fetchQuery = createAsyncThunk('extra/fetchQuery', async (query) => {
-    const response = await fetch(`https://saavn.dev/search/all?query=${query}`);
+    const response = await fetch(`https://saavn.dev/api/search/all?query=${query}`);
     const data = await response.json();
     return data;
   });
 
 // Fetch query song Data
 export const fetchQuerySong = createAsyncThunk('extra/fetchQuerySong', async (songLink) => {
-    const response = await fetch(`https://saavn.dev/songs?link=${songLink}`);
+    const response = await fetch(`https://saavn.dev/api/songs?link=${songLink}`);
     const data = await response.json();
     return data;
 });
 
 // Fetch query album Data
 export const fetchQueryAlbum = createAsyncThunk('extra/fetchQueryAlbum', async (albumLink) => {
-    const response = await fetch(`https://saavn.dev/albums?link=${albumLink}`);
+    const response = await fetch(`https://saavn.dev/api/albums?link=${albumLink}`);
     const data = await response.json();
     return data;
 });
 
 // Fetch query Playlist Data
 export const fetchQueryPlaylist = createAsyncThunk('extra/fetchQueryPlaylist', async (playlistId) => {
-    const response = await fetch(`https://saavn.dev/playlists?id=${playlistId}`);
+    const response = await fetch(`https://saavn.dev/api/playlists?id=${playlistId}`);
     const data = await response.json();
     return data;
 });
 
 // Fetch Songs Page Data
 export const fetchSongsPage = createAsyncThunk('extra/fetchSongsPage', async () => {
-    const response = await fetch('https://saavn.dev/playlists?id=1070105513');
+    const response = await fetch('https://saavn.dev/api/playlists?id=1070105513');
     const data = await response.json();
     return data;
 });
 
 // Fetch Home Page Data
 export const fetchHomePage = createAsyncThunk('extra/fetchHomePage', async () => {
-    const response = await fetch('https://saavn.dev/playlists?id=1053900960');
+    const response = await fetch('https://saavn.dev/api/playlists?id=1053900960');
     const data = await response.json();
     return data;
 });
 
 // Fetch Punjabi Modules Page Data : Album & Playlists
 export const fetchModules = createAsyncThunk('extra/fetchModules', async () => {
-    const response = await fetch('https://saavn.dev/modules?language=punjabi');
+    const response = await fetch('https://saavn.dev/api/modules?language=punjabi');
     const data = await response.json();
     return data;
 });
